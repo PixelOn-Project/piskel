f0a686d226ca8ba3a7ec1593005fa161008a229d
song200348@gmail.com <song200348@gmail.com>
2025-11-15 01:58:01 +0900

    style: PixelOn.js 스타일 변경

--- src/js/model/PixelOn.js
+++ src/js/model/PixelOn.js
@@ -4,22 +4,14 @@
   /**
    * PixelOn 애드온의 전체 상태를 관리하는 최상위 모델.
    */
-  ns.PixelOn = function (width, height, generate_count) {
-    if (width && height && generate_count != null) {
+  ns.PixelOn = function (width, height, generateCount) {
+    if (width && height && generateCount != null) {
       // --- 기본 설정 ---
       this.width = width;
       this.height = height;
-      this.generate_count = generate_count;
-      /**
-       * 생성된 모든 Image 객체를 저장하는 중앙 저장소 (Key-Value 형태).
-       * Key: Image UUID, Value: pskl.model.PixelOn.Image 인스턴스
-       */
+      this.generateCount = generateCount;
       this.imageStore = new Map();
-      /**
-       * AI 생성 세션 기록 목록.
-       * @type {Array<pskl.model.PixelOn.AiSession>}
-       */
-      this.sessions = [];
+      this.sessions = [];           
     } else {
       throw 'Missing arguments in PixelOn constructor : ' + Array.prototype.join.call(arguments, ',');
     }
@@ -35,7 +27,7 @@
     return this.height;
   };
   ns.PixelOn.prototype.getGenerateCount = function () {
-    return this.generate_count;
+    return this.generateCount;
   };
   ns.PixelOn.prototype.setWidth = function (width) {
     this.width = width;
@@ -44,36 +36,34 @@
     this.height = height;
   };
   ns.PixelOn.prototype.setGenerateCount = function (count) {
-    this.generate_count = count;
+    this.generateCount = count;
   };
   // 이미지 저장소에 이미지 추가/조회 메서드
-  ns.PixelOn.prototype.addImageToStore = function (image) {
+  ns.PixelOn.prototype.addImage = function (image) {
     // uuid 모듈을 사용하여 고유한 식별자 생성 후 imageStore에 저장
     const uuid = pskl.utils.Uuid.generate();
     this.imageStore.set(uuid, image);
     return uuid;
   };
-  ns.PixelOn.prototype.addImageToStore = function (uuid, image) {
-    return this.imageStore.set(uuid, image);
-  };
-  ns.PixelOn.prototype.getImageFromStore = function (uuid) {
+  ns.PixelOn.prototype.getImage = function (uuid) {
     return this.imageStore.get(uuid);
   };
-  // 초기 prompt와 spec을 받으면, 새로운 AiSession을 생성합니다.
-  ns.PixelOn.prototype.createSession = function (init_prompt, spec) {
-    const session = new pskl.model.PixelOn.AiSession(init_prompt, spec)
-    this.sessions.push(session);
-    return session;
+  ns.PixelOn.prototype.addSession = function (session) {
+    this.sessions.push(session)
   };
-  ns.PixelOn.prototype.getSessionList = function() {
-    return this.sessions
-  }
-  /**
-   * session의 index를 이용해 1개의 session을 Array에서 제거합니다.
-   * @param {Number} session_index 
-   */
-  ns.PixelOn.prototype.removeSessionList = function(session_index) {
-    if (session_index >= 0 && session_index < this.sessions.length)
-      this.sessions.splice(session_index, 1)
-  }
+  ns.PixelOn.prototype.getSessions = function() {
+    return this.sessions;
+  };
+  ns.PixelOn.prototype.getSessionAt = function(index) {
+    return this.sessions[index];
+  };
+  ns.PixelOn.prototype.removeSession = function (session) {
+    var index = this.sessions.indexOf(session);
+    if (index != -1) {
+      this.sessions.splice(index, 1);
+    }
+  };
+  ns.PixelOn.prototype.removeSessionAt = function(index) {
+    this.sessions.splice(index, 1);
+  };
 })();
